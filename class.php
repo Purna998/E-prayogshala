1e29dd96c21740b3a81b03f22ca9a5e1f6b85dbf
Purna Acharya <152753500+Purna998@users.noreply.github.com>
2024-04-27 17:05:27 +0545

    Add files via upload

---
+++ class.php
@@ -0,0 +1,154 @@
+<?php include "header.php"; ?>
+<section class="courses">
+   <h1 class="heading">Choose Your Class</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-2.jpg" alt="">
+            <div class="info">
+               <h3>Sharmila Shahi</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="images\Class 7.png" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">Class 7</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-3.jpg" alt="">
+            <div class="info">
+               <h3>Sharmila Shahi</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">Class 8</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-4.jpg" alt="">
+            <div class="info">
+               <h3>Purna Kant Acharya</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">Class 9</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-5.jpg" alt="">
+            <div class="info">
+               <h3>Purna Kant Acharya</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">Class 10</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-6.jpg" alt="">
+            <div class="info">
+               <h3>Adarsh Sharma</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete JQuery tutorial</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-7.jpg" alt="">
+            <div class="info">
+               <h3>Purna Kant Acharya</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete SASS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-8.jpg" alt="">
+            <div class="info">
+               <h3>Sharmila Shahi</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete PHP tutorial</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-9.jpg" alt="">
+            <div class="info">
+               <h3>Purna Kant Acharya</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete MySQL tutorial</h3>
+         <a href="playlist.html" class="inline-btn">View</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-1.jpg" alt="">
+            <div class="info">
+               <h3>Sharmila Shahi</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete react tutorial</h3>
+         <a href="playlist.html" class="inline-btn"><Var></Var>View</a>
+      </div>
+
+   </div>
+
+</section>
+<?php include "footer.php"; ?>
---
+++ index.php
@@ -0,0 +1,165 @@
+<?php include "header.php"; ?>
+<section class="home-grid">
+
+   <h1 class="heading">quick options</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3 class="title">likes and comments</h3>
+         <p class="likes">total likes : <span>25</span></p>
+         <a href="#" class="inline-btn">view likes</a>
+         <p class="likes">total comments : <span>12</span></p>
+         <a href="#" class="inline-btn">view comments</a>
+         <p class="likes">saved playlists : <span>4</span></p>
+         <a href="#" class="inline-btn">view playlists</a>
+      </div>
+
+      <div class="box">
+         <h3 class="title">top categories</h3>
+         <div class="flex">
+            <a href="#"><i class="fas fa-brain"></i><span>Science</span></a>
+            <a href="#"><i class="fas fa-robot"></i><span>Technology</span></a>
+            <a href="#"><i class="fas fa-user-secret"></i><span>Engineering</span></a>
+            <a href="#"><i class="fas fa-user-graduate"></i><span>Arts</span></a>
+            <a href="#"><i class="fas fa-calculator"></i><span>Maths</span></a>
+            <a href="#"><i class="fa fa-asl-interpreting"></i><span>Ubiquitious learning</span></a>
+            <a href="#"><i class="fas fa-rocket"></i><span>Emerging Technology</span></a>
+            <a href="#"><i class="fas fa-vial"></i><span>Practical Activity</span></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <h3 class="title">popular topics</h3>
+         <div class="flex">
+            <a href="#"><i class="fab fa-html5"></i><span>HTML</span></a>
+            <a href="#"><i class="fab fa-css3"></i><span>CSS</span></a>
+            <a href="#"><i class="fab fa-js"></i><span>javascript</span></a>
+            <a href="#"><i class="fab fa-react"></i><span>react</span></a>
+            <a href="#"><i class="fab fa-php"></i><span>PHP</span></a>
+            <a href="#"><i class="fab fa-bootstrap"></i><span>bootstrap</span></a>
+         </div>
+      </div>
+
+      <div class="box">
+         <h3 class="title">become a tutor</h3>
+         <p class="tutor">Join with us making student Smartly engage in STEAM through your Tutorials</p>
+         <a href="teachers.html" class="inline-btn">get started</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+<section class="courses">
+
+   <h1 class="heading">our courses</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-2.jpg" alt="">
+            <div class="info">
+               <h3>adasrh shaarma</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete HTML tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-3.jpg" alt="">
+            <div class="info">
+               <h3> adarsh Sharma</h3>
+               <span>21-10-2022</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete CSS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-4.jpg" alt="">
+            <div class="info">
+               <h3>adarsh Sharma</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete JS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-5.jpg" alt="">
+            <div class="info">
+               <h3>adarsh Sharma</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete Boostrap tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-6.jpg" alt="">
+            <div class="info">
+               <h3>Adarsh sharma</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete JQuery tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+      <div class="box">
+         <div class="tutor">
+            <img src="images/pic-7.jpg" alt="">
+            <div class="info">
+               <h3>Adarsh sharma</h3>
+               <span>21-10-2023</span>
+            </div>
+         </div>
+         <div class="thumb">
+            <img src="" alt="">
+            <span>10 videos</span>
+         </div>
+         <h3 class="title">complete SASS tutorial</h3>
+         <a href="playlist.html" class="inline-btn">view playlist</a>
+      </div>
+
+   </div>
+
+   <div class="more-btn">
+      <a href="courses.html" class="inline-option-btn">view all courses</a>
+   </div>
+
+</section>
+<?php include "footer.php"; ?>
